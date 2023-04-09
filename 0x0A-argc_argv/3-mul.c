7095fac7e69ef5960262d03e3450024a645d8520
kawtarabd <biouikawtar@gmail.com>
2023-04-09 15:28:32 -0700

    To infinity and beyond

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -20,20 +20,20 @@ int _atoi(char *s)
 	      len++ ;
 	while (i < len && f == 0)
 	{
-		if (s[i] == '-' )
+		if (s[i] == '-')
 			++d;
-		if (s[i] >= '0'&& s[i] <= '9')
+		if (s[i] >= '0' && s[i] <= '9')
 		{
 		digit = s[i] - '0';
 		if (d % 2)
-			digit = - digit;
+			digit = -digit;
 		n = n * 10 + digit;
 		f = 1;
 		if (s[i + 1] < '0' || s[i + 1] > '9')
 			break;
 		f = 0;
-		}
-		i++ ;
+} 
+i++ ;
 	}
 	if (f == 0)
 		return (0);
@@ -57,7 +57,7 @@ return (1);
 }
 num1 = _atoi(argv[1]);
 num2 = _atoi(argv[2]);
-result = num1 * num2;
+result = num1 *num2;
 
 printf("%d\n", result);
 return (0);
