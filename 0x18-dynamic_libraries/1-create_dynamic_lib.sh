71aa755b648dcc936e66d786507a98d078b721bf
kawtarabd <biouikawtar@gmail.com>
2023-11-17 13:06:09 -0800

    update

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-gcc -Wall -wextra -werror -pedantic -c -fPIC *.C
-gcc -shared -o liball.so *.0
+gcc -Wall -Wextra -Werror -pedantic -c -fPIC *.c
+gcc -shared -o liball.so *.o
 export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
